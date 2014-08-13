919bfe02d51c7161136b4d96e118e152f6882d20
Rob Wormald <rob.wormald@innitapps.com>
2014-08-13 01:08:59 -0500

    simple chat things

---
+++ api/controllers/MessageController.js
@@ -0,0 +1,21 @@
+/**
+ * MessageController
+ *
+ * @description :: Server-side logic for managing Messages
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `MessageController.body()`
+   */
+  body: function (req, res) {
+    return res.json({
+      todo: 'body() is not implemented yet!'
+    });
+  }
+};
+
